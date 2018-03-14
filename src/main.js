c53903ad4ec64527a53a7fd46b16f3c61218ac16
holyshatots <themcaffee@gmail.com>
2018-03-14 08:11:20 -0600

    Add bootstrap vue

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
 import App from './App'
 
 Vue.config.productionTip = false
+Vue.use(BootstrapVue)
+
+// Import CSS files for Bootstrap
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 /* eslint-disable no-new */
 new Vue({
