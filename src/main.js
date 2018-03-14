1bb846fc475c8802c0d68a918d85cf035c711c55
holyshatots <themcaffee@gmail.com>
2018-03-14 08:40:27 -0600

    Bootstrap + vue = <3

--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
+// Import CSS files for bootstrap
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
@@ -8,9 +12,6 @@ import router from './router'
 Vue.config.productionTip = false
 
 Vue.use(BootstrapVue)
-
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 /* eslint-disable no-new */
 new Vue({
