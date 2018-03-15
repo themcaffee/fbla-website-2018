0ee2c71105d317d9a7192a978775f55ec6ef92ee
holyshatots <themcaffee@gmail.com>
2018-03-15 00:14:58 -0600

    Add privacy policy text

--- src/components/PrivacyPolicy.vue
+++ src/components/PrivacyPolicy.vue
@@ -1,12 +1,50 @@
 <template>
   <div id="privacy-policy" class="container">
     <h1 id="privacy-title">Privacy Policy</h1>
-    <p>We don't steal stuff. Don't steal our stuff.</p>
+    <p>
+      Washed But Not Forgotten ("us", "we", or "our") operates the https://washed.mitchmcaffee.com website (the "Service").<br>
+      This page informs you of our policies regarding the collection, use and disclosure of Personal Information when you use our Service.<br>
+      We will not use or share your information with anyone except as described in this Privacy Policy.<br>
+      We use your Personal Information for providing and improving the Service. By using the Service, you agree to the collection and use of information in accordance with this policy. Unless otherwise defined in this Privacy Policy, terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, accessible at https://washed.mitchmcaffee.com<br>
+      <b>Information Collection And Use</b><br>
+      While using our Service, we may ask you to provide us with certain personally identifiable information that can be used to contact or identify you. Personally identifiable information may include, but is not limited to, your email address, other information ("Personal Information").<br>
+      <b>Log Data</b><br>
+      We collect information that your browser sends whenever you visit our Service ("Log Data"). This Log Data may include information such as your computer's Internet Protocol ("IP") address, browser type, browser version, the pages of our Service that you visit, the time and date of your visit, the time spent on those pages and other statistics.<br>
+      In addition, we may use third party services such as Google Analytics that collect, monitor and analyze this type of information in order to increase our Service's functionality. These third party service providers have their own privacy policies addressing how they use such information.<br>
+      <b>Cookies</b><br>
+      Cookies are files with small amount of data, which may include an anonymous unique identifier. Cookies are sent to your browser from a web site and stored on your computer's hard drive.<br>
+      We use "cookies" to collect information. You can instruct your browser to refuse all cookies or to indicate when a cookie is being sent. However, if you do not accept cookies, you may not be able to use some portions of our Service.<br>
+      <b>Service Providers</b><br>
+      We may employ third party companies and individuals to facilitate our Service, to provide the Service on our behalf, to perform Service-related services or to assist us in analyzing how our Service is used.<br>
+      These third parties have access to your Personal Information only to perform these tasks on our behalf and are obligated not to disclose or use it for any other purpose.<br>
+      <b>Communications</b><br>
+      We may use your Personal Information to contact you with newsletters, marketing or promotional materials and other information that may be of interest to you. You may opt out of receiving any, or all, of these communications from us by following the unsubscribe link or instructions provided in any email we send.<br>
+      <b>Security</b><br>
+      The security of your Personal Information is important to us, but remember that no method of transmission over the Internet, or method of electronic storage is 100% secure. While we strive to use commercially acceptable means to protect your Personal Information, we cannot guarantee its absolute security.<br>
+      <b>International Transfer</b><br>
+      Your information, including Personal Information, may be transferred to — and maintained on — computers located outside of your state, province, country or other governmental jurisdiction where the data protection laws may differ than those from your jurisdiction.<br>
+      If you are located outside India and choose to provide information to us, please note that we transfer the information, including Personal Information, to India and process it there.<br>
+      Your consent to this Privacy Policy followed by your submission of such information represents your agreement to that transfer.<br>
+      <b>Links To Other Sites</b><br>
+      Our Service may contain links to other sites that are not operated by us. If you click on a third party link, you will be directed to that third party's site. We strongly advise you to review the Privacy Policy of every site you visit.<br>
+      We have no control over, and assume no responsibility for the content, privacy policies or practices of any third party sites or services.<br>
+      <b>Children's Privacy</b><br>
+      Our Service does not address anyone under the age of 13 ("Children").<br>
+      We do not knowingly collect personally identifiable information from children under 13. If you are a parent or guardian and you are aware that your Children has provided us with Personal Information, please contact us. If we become aware that we have collected Personal Information from a children under age 13 without verification of parental consent, we take steps to remove that information from our servers.<br>
+      <b>Changes To This Privacy Policy</b><br>
+      We may update our Privacy Policy from time to time. We will notify you of any changes by posting the new Privacy Policy on this page.<br>
+      You are advised to review this Privacy Policy periodically for any changes. Changes to this Privacy Policy are effective when they are posted on this page.<br>
+      <b>Contact Us</b><br>
+      If you have any questions about this Privacy Policy, please contact us at support[at]washed.mitchmcaffee.com or via the contact form.<br>
+    </p>
   </div>
 </template>
 
 <style>
 #privacy-title {
-  padding-top: 2em;
+  padding-top: 1em;
+}
+#privacy-policy {
+  padding-bottom: 4em;
 }
 </style>
