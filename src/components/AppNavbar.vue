2cdfdf9cc9bbe0e7e19580d38b5f0c9b1ea40752
holyshatots <themcaffee@gmail.com>
2018-03-14 23:46:37 -0600

    Fix links

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -7,7 +7,7 @@
     <b-navbar toggleable="md" type="dark" variant="info">
       <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
 
-    <b-navbar-brand href="#" class="display-1">Washed But Not Forgotten</b-navbar-brand>
+    <b-navbar-brand :to="{name: 'MainPage'}" class="display-1">Washed But Not Forgotten</b-navbar-brand>
 
       <!-- <b-collapse is-nav id="nav_collapse">
 
